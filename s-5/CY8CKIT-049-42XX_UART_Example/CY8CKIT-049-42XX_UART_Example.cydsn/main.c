01ad2a3c480a9e0234364a5ac222debc442d0c4c
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-07-12 06:40:42 +0900

    R.173
    s-5#1
    ---
    1. "Programming with the UART Bootloader"
    
        ==> done

---
+++ s-5/CY8CKIT-049-42XX_UART_Example/CY8CKIT-049-42XX_UART_Example.cydsn/main.c
@@ -0,0 +1,27 @@
+
+#include <project.h>
+
+const char welcome_string[] = "Hello World!\n\r";
+
+int main()
+{
+	uint8_t i;
+	char c;
+	
+    UART_Start();                             // start the UART peripheral
+	
+	while(SW1_Read());                        // wait for button press to continue (gives you time to switch to a terminal window)
+
+	for(i=0; i<strlen(welcome_string); i++) { // display the welcome string
+		UART_UartPutChar(welcome_string[i]);
+	}
+    for(;;) {
+		c = UART_UartGetChar();               // get typed character
+        if(c) {                               // if it's a valid character
+			UART_UartPutChar(c);              // echo it in the terminal window
+		}
+    }
+    
+    return 0;
+    
+}
