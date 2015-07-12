be56f034cd867188eb9de3d711d418ab78d15ee7
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-07-13 08:52:42 +0900

    R.173
    s-7#1
    ---
    1. onboard LED => on/off
    
        ==> done

---
+++ s-7/LED_onoff.cydsn/main.c
@@ -0,0 +1,30 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include <project.h>
+
+int main()
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+
+    for(;;)
+    {
+        /* Place your application code here. */
+        LED_Write( ! LED_Read());
+        
+        //CyDelay( 500 );
+        CyDelay( 1000 );
+    }
+}
+
+/* [] END OF FILE */
