471b7068a5cdfb302056b04edcde69d70c100f7d
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-07-12 17:31:59 +0900

    R.173
    s-6#1
    ---
    1. "PWM Example"
    
        ==> done

---
+++ s-6/CY8CKIT-049-42XX_PWM_Example/CY8CKIT-049-42XX_PWM_Example.cydsn/main.c
@@ -0,0 +1,22 @@
+
+#include <project.h>
+
+volatile uint8_t inc_flag = 1;
+volatile uint16_t compare_val = 0;
+
+int main()
+{
+    PWM_Start();                                  // start the PWM peripheral
+	
+    for(;;) {
+		if(compare_val > 11000) { inc_flag = 0; } // set increment flag to false
+		if(compare_val < 1000) { inc_flag = 1; }  // set increment flag to true
+		if(inc_flag) {                            // if increment flag is true
+			compare_val += 500;                   // increment compare value
+		}else{                                    // else if increment flag is false
+			compare_val -= 500;                   // decrement compare value
+		}
+		PWM_WriteCompare(compare_val);            // write new compare value to PWM
+		CyDelay(25);                              // wait 25ms before looping again
+    }
+}
