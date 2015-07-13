c86b2015250a9ea570dbe18ed9a89206706e80aa
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-07-13 09:09:24 +0900

    R.173
    s-7#3
    ---
    1. push switch --> LED starts blinking
    	==> push again --> LED starts keeping on
    
        ==> done

--- s-7/LED_onoff.cydsn/main.c
+++ s-7/LED_onoff.cydsn/main.c
@@ -17,10 +17,33 @@ int main()
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
 
+//    int sw_state;
+
+    int sw_read;
+    
     for(;;)
     {
+        /* switch state */
+        sw_read = SW1_Read();
+        
+        if (sw_read == 1) {
+
+        	LED_Write( 1 );
+
+        	continue;
+
+		}
+
         /* Place your application code here. */
-        LED_Write( SW1_Read() );
+//        LED_Write( SW1_Read() );
+
+        LED_Write( 1 );
+
+        CyDelay( 500 );
+
+        LED_Write( 0 );
+
+        CyDelay( 500 );
         
         //CyDelay( 500 );
         //CyDelay( 1000 );
